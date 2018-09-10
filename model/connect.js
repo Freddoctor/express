40069e60070acfa5a33b87d9d4a19318ec2dcbdf
Freddoctor <642582009@qq.com>
2018-09-10 14:40:09 +0800

    异步处理

--- model/connect.js
+++ model/connect.js
@@ -1,13 +1,21 @@
+const amqp = require("amqplib");
 const express = require('express');
 const app = express();
 
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http,{
+  pingInterval: 25000,
+  pingTimeout: 60000,
+});
 
-const amqp = require('amqplib');
+{ transports: ['websocket'] }
+
+const queue = "quotationQuene2";
 
 app.disable('x-powered-by');
 
+var connection;
+// 连接RabbitMQ
 const options = {
   hostname: '115.159.200.179',
   port: 5672,
@@ -16,51 +24,47 @@ const options = {
   password: 'guest'
 };
 
-amqp.connect(options).then((conn) => {
-  try {
-    spread(conn);
-  } catch (e) {
-    console.log(e)
+ const info = {
+  "durable": true,
+  "auto_delete": false,
+  "durable": true,
+  "arguments": {
+    "x-message-ttl": 10000,
+    "x-max-length": 1000
   }
-});
-
-function spread(conn) {
-  conn.createChannel().then((ch) => {
-    try {
-      accepTo(ch, "quotationQuene2");
-    } catch (e) {
-      console.log(e)
-    }
-  })
 }
 
-process.on('uncaughtException', function(err) {
-  //打印出错误
-  console.log("捕获一个错误:" + err);
-});
-
-function accepTo(ch, q) {
-  let options = {};
-  //"durable": true, "arguments": {  "x-message-ttl": 10000,"x-max-length": 1000 }
+async function connectRabbitMQ() {
   try {
-    ch.consume(q, (msg) => {
-      try {
-        console.log(msg.content.toString())
-        if (msg !== null) {
-          io.emit('chat message', msg.content.toString());
-          ch.ack(msg);
-        }
-      } catch (e) {
-        console.log(e)
+    connection = await amqp.connect(options);
+    console.info("connect to RabbitMQ success");
+    const channel = await connection.createChannel();
+    await channel.assertQueue(queue,info);
+    await channel.consume(queue, async function(message) {
+      // console.log(message.content.toString());
+      if (message !== null) {
+        io.emit('chat message', message.content.toString());
+        channel.ack(message);
       }
-    }, options);
-  } catch (e) {
-    console.log(e)
+    });
+    connection.on("error", function(err) {
+      console.log(err);
+      setTimeout(connectRabbitMQ, 10000);
+    });
+    connection.on("close", function() {
+      console.error("connection to RabbitQM closed!");
+      setTimeout(connectRabbitMQ, 10000);
+    });
+  } catch (err) {
+    console.error(err);
+    setTimeout(connectRabbitMQ, 10000);
   }
 }
 
 http.listen(3000, function() {
   console.log('listening on *:3000'); //io接口
 });
+
+connectRabbitMQ();
 
 module.exports = app;
